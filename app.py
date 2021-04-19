d549bd547fa5ff9192ca9195b351dfc3e8408510
Anne <a.wieggers@hotmail.com>
2021-04-19 10:47:06 +1000

    updated app.py remove sqllite

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ app = Flask(__name__)
 #################################################
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') 
+# or "sqlite:///db.sqlite"
 
 db = SQLAlchemy(app)
 
